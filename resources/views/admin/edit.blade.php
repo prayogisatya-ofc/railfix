20e1a1324a92394ccd1e5731654f00fa56dbe01a
Rama Wijaya <ramawijaya518yh@gmail.com>
2025-05-21 10:06:03 +0700

    Add: Frontend Admin

---
+++ resources/views/admin/edit.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('title', 'Edit Data')
+
+@section('content')
+    <div class="pc-content">
+        <div class="page-header m-0">
+            <div class="page-block">
+                <div class="row align-items-center">
+                    <div class="col-md-12">
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('location.index') }}">Data Lokasi</a></li>
+                            <li class="breadcrumb-item"><span>Edit Data</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3 align-items-center pt-2">
+            <div class="col-md-4">
+                <h4 class="mb-3 mb-md-0">Edit Data</h4>
+            </div>
+            <div class="col-md-8">
+                <div class="float-start float-md-end">
+                    <a href="" class="btn btn-outline-secondary me-2">Kembali</a>
+                    <button type="submit" form="form-tambah" class="btn btn-primary">Update</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <form action="" method="post" id="form-tambah">
+                    @csrf
+                    @method('put')
+                    <div class="mb-3">
+                        <label class="form-label">Nama Lokasi <span class="text-danger">*</span></label>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror"
+                            placeholder="Ex: Stasiun Rejosari" aria-label="Nama" aria-describedby="button-addon2"
+                            name="name" value="">
+                        @error('name')
+                            <small class="text-danger mt-1" role="alert">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Email <span class="text-danger">*</span></label>
+                        <input type="email" class="form-control @error('name') is-invalid @enderror"
+                            placeholder="Ex: abc@gmail.com" aria-label="Email" aria-describedby="button-addon2"
+                            name="name" value="">
+                        @error('name')
+                            <small class="text-danger mt-1" role="alert">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
+                    <div >
+                        <label class="form-label">Password <span class="text-danger">*</span></label>
+                        <input type="password" class="form-control @error('name') is-invalid @enderror"
+                            placeholder="Ex: 123Stasiun" aria-label="Nama" aria-describedby="button-addon2"
+                            name="name" value="">
+                        @error('name')
+                            <small class="text-danger mt-1" role="alert">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.app')
+
+@section('title', 'admin')
+
+@section('content')
+    <div class="pc-content">
+        <div class="page-header m-0">
+            <div class="page-block">
+                <div class="row align-items-center">
+                    <div class="col-md-12">
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">Home</a></li>
+                            <li class="breadcrumb-item"><span>Data Admin</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row pt-2">
+            <div class="col-md-12">
+                <h4 class="mb-3">Data Admin</h4>
+                @if (session('success'))
+                    <div class="alert alert-success alert-dismissible" role="alert">
+                        {!! session('success') !!}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+                <div class="card">
+                    <div class="card-header ">
+                        <form action="" method="get">
+                            <div class="row align-items-center">
+                                <div class="col-md-4 mb-3 mb-md-0">
+                                    <div class="input-group">
+                                        <input type="search" class="form-control" placeholder="Cari" aria-label="Nama"
+                                            aria-describedby="button-addon2" name="search" value="">
+                                        <button class="btn btn-outline-primary" type="submit"
+                                            id="button-addon2">Search</button>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <a href="" class="btn btn-primary float-start float-md-end">Tambah</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-body pt-0">
+                        <div class="table-responsive">
+                            <table class="table table-hover text-nowrap mb-0">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama </th>
+                                        <th>Email</th>
+                                        <th class="text-end">Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {{-- @foreach ($admins as $admin)
+                                        <tr>
+                                            <td>{{ ($admins->currentPage() - 1) * $admins->perPage() + $loop->iteration }}
+                                            </td>
+                                            <td>{{ $location->name }}</td>
+                                            <td class="text-end">
+                                                <a href="{{ route('admin.edit', $admin->id) }}"
+                                                    class="btn btn-sm btn-warning">
+                                                    <i class="ti ti-edit"></i>
+                                                </a>
+
+                                                <form action="{{ route('admin.destroy', $admin->id) }}" method="POST"
+                                                    style="display:inline-block;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-sm btn-danger"
+                                                        onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')">
+                                                        <i class="ti ti-trash"></i>
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach --}}
+                                </tbody>
+                            </table>
+                        </div>
+                        {{-- <div class="mt-3">{{ $locations->links() }}</div> --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
