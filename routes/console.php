99fb8a02c6855f7b2fa36191faac178b5b245512
prayogisatya-ofc <besutodio@gmail.com>
2025-06-23 14:21:07 +0700

    update: schedule hourly

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ use Illuminate\Support\Facades\Schedule;
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote');
 
-Schedule::command('notif:check-inventory')->everyTenMinutes();
+Schedule::command('notif:check-inventory')->hourly();
